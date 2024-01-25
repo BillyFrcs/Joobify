6fdf472a4f03b4ee2368c6e0ebddd27d5c781f62
Billy Franscois <BillyFranscois@outlook.com>
2024-01-26 00:42:28 +0900

    Update not-found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -9,12 +9,21 @@ export const metadata = {
 
 function NotFound() {
     return (
-        <div>
-            <h1>404 - That page does not seem to exist...</h1>
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <iframe
+                src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
+                width="480"
+                height="362"
+                frameBorder="0"
+                allowFullScreen
+            ></iframe>
 
-            <Button className='btn-style light-font' color='failure'>
-                <Link href="/">Go Home</Link>
-            </Button>
+            <h1 className='mt-5 text-4xl font-bold'>404 - Not Found</h1>
+            <p className='text-xl'>The page you are looking for does not exist.</p>
+
+            <Link className='mt-5' href="/">
+                <Button className='btn-style light-font' color='failure'>Go Home</Button>
+            </Link>
         </div>
     );
 }
