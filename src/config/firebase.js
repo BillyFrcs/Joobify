2a882d510702cf9dea868f8512cd4da071ed361d
Billy Franscois <BillyFranscois@outlook.com>
2024-01-26 19:03:49 +0900

    Update firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
+// import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+
+// const analytics = getAnalytics(firebaseApp);
 
 export default firebaseApp;
