aa6d4ae38ef464a0a1d634e158d64d07b62ddea0
Billy Franscois <BillyFranscois@outlook.com>
2024-01-30 01:25:25 +0900

    Add axios

---
+++ src/utils/axios.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: process.env.JOOBIFY_ENDPOINT,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    }
+});
+
+/*
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+    (config) => {
+        // You can do something with the request before it is sent
+        // For example, add authentication token
+        // config.headers.Authorization = `Bearer ${getToken()}`;
+        return config;
+    },
+    (error) => {
+        // Handle request error
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+    (response) => {
+        // You can do something with the response data
+        return response;
+    },
+    (error) => {
+        // Handle response error
+        return Promise.reject(error);
+    }
+);
+*/
+
+export default axiosInstance;
