7366d515404b77bafa89baad2ac7b9eec363139b
Billy Franscois <BillyFranscois@outlook.com>
2024-01-30 01:26:25 +0900

    Implemented signin and signup api

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -9,26 +9,93 @@ import { Button } from 'flowbite-react';
 
 import GoogleAuth from './googleauth';
 
+import { joobifyEndpoint } from '@/utils/api';
+
 const SignInForm = () => {
+    const router = useRouter();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setEmail('');
+        setPassword('');
+    }, []);
+
+    // Form data
+    const [formData, setFormData] = useState({
+        // take from the input form
+        email: email,
+        password: password
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`${joobifyEndpoint}/auth/signin`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                // get the specific user data
+                console.log(result.data.user.stsTokenManager.accessToken);
+
+                // set the user data in the local storage
+                // localStorage.setItem('user', JSON.stringify(data));
+
+                // Signup successful, redirect or show success message
+                // router.push('/');
+            } else if (response.status === 500) {
+                setError(result.message);
+
+                setIsVisible(true);
+
+                // set the timeout
+                setTimeout(() => {
+                    setIsVisible(false);
+                }, 5000);
+
+                console.log(result.message);
+            }
+        } catch (error) {
+            console.error('Error occurred: ', error);
+        }
+    };
+
     return (
         <main className='flex flex-col md:order-2 ml-9 pl-5 pt-10'>
             <h1 className="font-bold black-color text-4xl">WELCOME</h1>
             <h1 className="font-bold black-color text-4xl">TO <span className="main-color">JOOBIFY</span></h1>
 
-            <form method='POST' className="mt-6 grid grid-flow-row justify-start">
+            <form method='POST' onSubmit={handleSubmit} className="mt-6 grid grid-flow-row justify-start">
                 <div className="mb-4">
                     <label htmlFor="email" className="black-color w-80 block mb-2 text-sm font-medium text-gray-900 dark:text-white light-font">Email address</label>
-                    <input type="email" id="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required={true} />
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required={true} />
                 </div>
 
                 <div className="mb-4">
                     <label htmlFor="password" className="black-color w-80 block mb-2 text-sm font-medium text-gray-900 dark:text-white light-font">Your password</label>
-                    <input type="password" autoComplete="on" id="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required={true} />
+                    <input type="password" autoComplete="on" id="password" name="password" value={formData.password} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required={true} />
                 </div>
 
                 <Link className="forgot-password" href="/resetpassword">Forgot password?</Link>
 
-                {/* <span className='text-red-500 mt-2'>Invalid email or password, please try again!</span> */}
+                {isVisible && <span className='text-sm text-red-500 mt-2'>{error}</span>}
 
                 <Button type="submit" className="joobify-main-color mt-3 btn-style light-font" pill>
                     Sign in
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -8,40 +8,100 @@ import { useRouter } from 'next/navigation';
 import { Button } from 'flowbite-react';
 
 import GoogleAuth from './googleauth';
+import { joobifyEndpoint } from '@/utils/api';
 
 const SignUpForm = () => {
+    const router = useRouter();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setName('');
+        setEmail('');
+        setPassword('');
+    }, []);
+
+    // Form data
+    const [formData, setFormData] = useState({
+        // take from the input form
+        name: name,
+        email: email,
+        password: password
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`${joobifyEndpoint}/auth/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+
+                // Signup successful, redirect or show success message
+                router.push('/');
+            } else if (response.status === 500) {
+                setError(result.error);
+
+                setIsVisible(true);
+
+                // set the timeout
+                setTimeout(() => {
+                    setIsVisible(false);
+                }, 5000);
+
+                console.log(result.error);
+            }
+        } catch (error) {
+            console.error('Error occurred: ', error);
+        }
+    };
+
     return (
         <main className='flex flex-col md:order-2 ml-9 pl-5 pt-10'>
             <h1 className="font-bold black-color text-4xl">JOIN TO</h1>
             <h1 className="font-bold black-color text-4xl"><span className="main-color">JOOBIFY</span> NOW</h1>
 
-            <form method='POST' className="mt-6 grid grid-flow-row justify-start">
+            <form method='POST' onSubmit={handleSubmit} className="mt-6 grid grid-flow-row justify-start">
                 <div className="mb-4">
                     <label htmlFor="name" className="black-color w-80 block mb-2 text-sm font-medium text-gray-900 dark:text-white light-font">Full name</label>
-                    <input type="text" id="name" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required={true} />
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required={true} />
                 </div>
 
                 <div className="mb-4">
                     <label htmlFor="email" className="black-color w-80 block mb-2 text-sm font-medium text-gray-900 dark:text-white light-font">Email address</label>
-                    <input type="email" id="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required={true} />
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required={true} />
                 </div>
 
                 <div className="mb-4">
                     <label htmlFor="password" className="black-color w-80 block mb-2 text-sm font-medium text-gray-900 dark:text-white light-font">Your password</label>
-                    <input type="password" autoComplete="on" id="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required={true} />
+                    <input type="password" autoComplete="on" id="password" name="password" value={formData.password} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required={true} />
                 </div>
 
-                {/* <span className='text-red-500 mb-2'>Invalid email or password, please try again!</span> */}
+                {isVisible && <span className='text-sm text-red-500 mt-2'>{error}</span>}
 
                 <Button type="submit" className="joobify-main-color mt-3 btn-style light-font" pill>
                     Sign up
                 </Button>
 
                 <span className='mt-2 text-center black-color new-to-joobify light-font'>or</span>
-
-                {/* <Button type='button' className="mt-2 btn-style light-font" color="light" pill>
-                    <FcGoogle className='mr-2' /> Continue with Google
-                </Button> */}
 
                 <GoogleAuth />
 
